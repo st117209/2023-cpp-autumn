a4ca02d8922b52584f57d902fcf1ad18cea81aea
st117209 <st117209@student.spbu.ru>
2023-12-11 17:21:36 +0300

    Update 7.cpp

--- 2023.11.22-homework-6/Project7/7.cpp
+++ 2023.11.22-homework-6/Project7/7.cpp
@@ -8,7 +8,7 @@ void shift(int from = 1, int to = 3)
 	std::cout << d[from][n[from] - 1] << " " << from << " " << to << std::endl;
 	--n[from];
 	d[to][n[to]] = d[from][n[from]];
-	n[from]++;
+	n[to]++;
 }
 
 void hanoi(int cnt, int from = 1, int to = 3)
@@ -22,24 +22,26 @@ void hanoi(int cnt, int from = 1, int to = 3)
 
 	hanoi(cnt - 1, from, res);
 	shift(from, to);
-	printf("%d %d %d\n", cnt, from, to);
+	//printf("%d %d %d\n", cnt, from, to);
 	hanoi(cnt - 1, res, to);
 }
 
 void sort()
 {
-	int cnt = 1;
-	while(n[1] > 0)
+	int cnt = 0;
+	while (n[1] > 0)
 	{
-		for (int i = n[3]; i > 0; --i)
+		for (int i = 0; i < n[3]; ++i)
 		{
-			if (d[1][n[1] - 1] < d[3][i - 1])
+			if (d[1][n[1] - 1] > d[3][n[3] - 1 - i])
 			{
 				cnt = n[3] - i;
 				break;
 			}
 		}
-		hanoi(cnt);
+		hanoi(cnt, 3, 2);
+		shift(1, 3);
+		hanoi(cnt, 2, 3);
 		cnt = 0;
 	}
 }
@@ -55,3 +57,14 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+/* {
+	for (int i = 1; i <= n[3]; ++i)
+	{
+		if (d[1][n[1] - 1] > d[3][n[3] - i])
+		{
+			cnt++;
+		}
+		else { break; }
+	}
+}*/
